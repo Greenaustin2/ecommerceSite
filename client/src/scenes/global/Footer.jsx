37b91742b8c6ce4eed9e6bf9c1b6fae06d54fbda
Greenaustin2 <greenaustin2@gmail.com>
2023-07-19 16:06:07 -0400

    stripe integration

---
+++ client/src/scenes/global/Footer.jsx
@@ -0,0 +1,71 @@
+import { useTheme } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { shades } from "../../theme";
+
+const Footer = () => {
+  const {
+    palette: { neutral },
+  } = useTheme();
+  return (
+    <Box mt="70px" p="40px 0" backgroundColor={neutral.light}>
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="spaceBetween"
+        flexWrap="wrap"
+        rowGap="30px"
+        columnGap="clamp(20px, 30px, 40px)"
+      >
+        <Box width="clamp(20%, 30%, 40%)">
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            mb="30px"
+            color={shades.secondary[500]}
+          >
+            ECOMMER
+          </Typography>
+          <div>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt.
+          </div>
+        </Box>
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            About Us
+          </Typography>
+          <Typography mb="30px">Careers</Typography>
+          <Typography mb="30px">Our Story</Typography>
+          <Typography mb="30px">Terms and Conditions</Typography>
+          <Typography mb="30px">Privacy Policy</Typography>
+        </Box>
+        <Box>
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Customer Care
+          </Typography>
+          <Typography mb="30px">Help Center</Typography>
+          <Typography mb="30px">Track Your Order</Typography>
+          <Typography mb="30px">Returns and Refunds</Typography>
+          <Typography mb="30px">Corporate & Bulk Purchasing</Typography>
+        </Box>
+        <Box width="clamp(20%, 25%, 30%)">
+          <Typography variant="h4" fontWeight="bold" mb="30px">
+            Contact Us
+          </Typography>
+          <Typography mb="30px">
+            0000 Unknown Blvd, United States 00000
+          </Typography>
+          <Typography mb="30px">Email: emailaddress@ecomm.com</Typography>
+          <Typography mb="30px">(000) 000-0000</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
